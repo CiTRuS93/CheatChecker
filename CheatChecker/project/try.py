8f7db035a26f9453999468d4a795c303cf5327fc
Igor <igoralikin@gmail.com>
2020-09-13 16:52:32 +0300

    questions

--- CheatChecker/project/try.py
+++ CheatChecker/project/try.py
@@ -26,18 +26,18 @@ Y_set = {w for w in Y_list}
 rvector = X_set.union(Y_set)
 
 for w in rvector:
-	if w in X_set:
-		l1.append(1)  # create a vector
-	else:
-		l1.append(0)
-	if w in Y_set:
-		l2.append(1)
-	else:
-		l2.append(0)
+    if w in X_set:
+        l1.append(1)  # create a vector
+    else:
+        l1.append(0)
+    if w in Y_set:
+        l2.append(1)
+    else:
+        l2.append(0)
 c = 0
 
 # cosine formula
 for i in range(len(rvector)):
-	c += l1[i]*l2[i]
+    c += l1[i]*l2[i]
 cosine = c / float((sum(l1)*sum(l2))**0.5)
 print("similarity: ", cosine)
